5bee0ecf38b4e1f6018e37d7ccbd5a23105e8423
Adoni Cristian <adoni.cristiang@gmail.com>
2022-11-16 22:22:05 -0400

    added data base info

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -10,9 +10,11 @@ function cartReducer(state, action) {
   if (action.type === "ADD") {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
+
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
@@ -32,13 +34,18 @@ function cartReducer(state, action) {
       totalAmount: updatedTotalAmount,
     };
   }
+
   if (action.type === "REMOVE") {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
+
     const existingItem = state.items[existingCartItemIndex];
+
     const updatedTotalAmount = state.totalAmount - existingItem.price;
+
     let updatedItems;
+
     if (existingItem.amount === 1) {
       updatedItems = state.items.filter((item) => item.id !== action.id);
     } else {
@@ -52,6 +59,7 @@ function cartReducer(state, action) {
       totalAmount: updatedTotalAmount,
     };
   }
+
   return defaultCartState;
 }
 
